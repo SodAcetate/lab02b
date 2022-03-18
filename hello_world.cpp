158c23e7e547e60a66630fd3a16881f9c5fe1b33
SodAcetate <sodium.acetate.82@gmail.com>
2022-03-18 11:50:39 +0300

    updated hello_world.cpp to ask for name

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,9 @@
 include <iostream>
+include <string>
 using namespace std;
 int main() {
-cout << "Hello world!";
+string name;
+cout << "PLease enter name";
+cin >> name;
+cout << "Hello world from " << name;
 }
